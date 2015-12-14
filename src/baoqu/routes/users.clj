794bef1d99756ebf60eb9a35b77b2f9f3954e0a8
Mario Garcia <mario.ggar@gmail.com>
2015-12-14 18:31:09 +0100

    Add changes to call users persistence layer

--- src/baoqu/routes/users.clj
+++ src/baoqu/routes/users.clj
@@ -1,12 +1,12 @@
-(ns baoqu.routes.users
-  (:require [cheshire.core :as json]
-            [baoqu.persis.users :as db]))
-
 ;;  _   _ ___  ___ _ __ ___
 ;; | | | / __|/ _ \ '__/ __|
 ;; | |_| \__ \  __/ |  \__ \
 ;;  \__,_|___/\___|_|  |___/
 ;;
+
+(ns baoqu.routes.users
+  (:require [cheshire.core :as json]
+            [baoqu.persis.users :as db]))
 
 (defn create
   "Creates a new user"
@@ -16,5 +16,4 @@
         age (:age json)
         saved (db/create {:name name
                           :age age})]
-    (json/generate-string json))
-  )
+    (json/generate-string saved)))
